a1860a7474ee74e286d87fa3335acc527a3e587e
Stephen Algeo <stephen.algeo@gmail.com>
2016-07-18 13:47:52 +0100

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,14 @@
+</div><!-- .site-content -->
+
+<footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="site-info">
+        Funko Rocks!!
+    </div><!-- .site-info -->
+</footer><!-- .site-footer -->
+
+</div><!-- .site -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
